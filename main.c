5ed30ae3f3e7bb822deac3d1ef4057da94528140
Daniel Dakhno <dakhnod@gmail.com>
2023-03-02 02:58:11 +0100

    made error message more verbose

--- main.c
+++ main.c
@@ -130,14 +130,32 @@ void assert_nrf_callback(uint16_t line_num, const uint8_t *p_file_name)
 void app_error_fault_handler(uint32_t id, uint32_t pc, uint32_t info)
 {
     // On assert, the system can only recover with a reset.
-#ifndef DEBUG
-    NVIC_SystemReset();
-#else
-    error_info_t *error_info = (error_info_t*)info;
-    NRF_LOG_ERROR("error: %d\n", error_info->err_code);
-    for(;;){
-        NRF_LOG_PROCESS();
-    }
+  unsigned int tmp = id;
+  NRF_LOG_ERROR("app_error_print():\r\n");
+  NRF_LOG_ERROR("Fault identifier:  0x%X\r\n", tmp);
+  NRF_LOG_ERROR("Program counter:   0x%X\r\n", tmp = pc);
+  NRF_LOG_ERROR("Fault information: 0x%X\r\n", tmp = info);
+
+  switch (id) {
+    case NRF_FAULT_ID_SDK_ASSERT:
+      NRF_LOG_ERROR("Line Number: %u\r\n", tmp =
+        ((assert_info_t *)(info))->line_num);
+      NRF_LOG_ERROR("File Name:   %s\r\n",
+        (uint32_t)((assert_info_t *)(info))->p_file_name);
+      break;
+
+    case NRF_FAULT_ID_SDK_ERROR:
+      NRF_LOG_ERROR("Line Number: %u\r\n", tmp =
+        ((error_info_t *)(info))->line_num);
+      NRF_LOG_ERROR("File Name:   %s\r\n",
+        (uint32_t)((error_info_t *)(info))->p_file_name);
+      NRF_LOG_ERROR("Error Code:  0x%X\r\n", tmp =
+        ((error_info_t *)(info))->err_code);
+      NRF_LOG_ERROR("Error description: %s\n", (uint32_t)ERR_TO_STR(((error_info_t *)(info))->err_code));
+      break;
+  }
+#ifdef DEBUG
+  for (;;);
 #endif
 }
 
